353d7cbd9b83f5eec7f0f650a58ee45cc5044a76
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2024-11-30 17:54:44 +0500

    Created Item class. It has 3 child classes for armor, food and weapon type items

---
+++ Item.cpp
@@ -0,0 +1,109 @@
+#include "include/Item.hpp"
+
+// Item class
+Item::Item() {
+    name = " ";
+    price = -1;
+    ID = 0;
+}
+
+Item::Item(string name, int price, int ID) : name(name), price(price), ID(ID) {}
+
+void Item::setName(string name) {
+    this->name = name;
+}
+
+void Item::setPrice(int price) {
+    this->price = price;
+}
+
+void Item::setID(int ID) {
+    this->ID = ID;
+}
+
+string Item::getName() {
+    return name;
+}
+
+int Item::getPrice() {
+    return price;
+}
+
+int Item::getID() {
+    return ID;
+}
+
+// Armor class
+Armor::Armor() {
+    type = HELMET;
+    defense = -1;
+}
+
+Armor::Armor(string name, int price, int ID, Type type, int defense) :
+        Item(name, price, ID), type(type), defense(defense) {}
+
+void Armor::setType(Type type) {
+    this->type = type;
+}
+
+void Armor::setDefense(int defense) {
+    this->defense = defense;
+}
+
+Armor::Type Armor::getType() {
+    return type;
+}
+
+int Armor::getDefense() {
+    return defense;
+}
+
+// Food class
+Food::Food() {
+    int health = -1;
+}
+
+Food::Food(string name, int price, int ID, int health) :
+        Item(name, price, ID), health(health) {}
+
+void Food::setHealth(int health) {
+    this->health = health;
+}
+
+int Food::getHealth() {
+    return health;
+}
+
+// Weapon class
+Weapon::Weapon() {
+    type = SWORD;
+    attack = -1;
+    speed = -1;
+}
+
+Weapon::Weapon(string name, int price, int ID, Type type, int attack, int speed) : 
+        Item(name, price, ID), type(type), attack(attack), speed(speed) {}
+
+void Weapon::setType(Type type) {
+    this->type = type;
+}
+
+void Weapon::setAttack(int attack) {
+    this->attack = attack;
+}
+
+void Weapon::setSpeed(int speed) {
+    this->speed = speed;
+}
+
+Weapon::Type Weapon::getType() {
+    return type;
+}
+
+int Weapon::getAttack() {
+    return attack;
+}
+
+int Weapon::getSpeed() {
+    return speed;
+}
--- include/Item.hpp
+++ include/Item.hpp
@@ -1,8 +1,68 @@
 #pragma once
+#include <iostream>
+using namespace std;
 
 class Item {
-private:
+protected:
+    string name;
+    int price;
+    int ID;
 
 public:
+    Item();
+    Item(string name, int price, int ID);
+    void setName(string name);
+    void setPrice(int price);
+    void setID(int ID);
+    string getName();
+    int getPrice();
+    int getID();
+};
 
+class Armor : public Item {
+public:
+    enum Type {HELMET, CHESTPLATE, LEGGINGS, BOOTS};
+
+private:
+    Type type;
+    int defense;
+
+public:
+    Armor();
+    Armor(string name, int price, int ID, Type type, int defense);
+    void setType(Type type);
+    void setDefense(int defense);
+    Type getType();
+    int getDefense();
+};
+
+class Food : public Item {
+private:
+    int health;
+
+public:
+    Food();
+    Food(string name, int price, int ID, int health);
+    void setHealth(int health);
+    int getHealth();
+};
+
+class Weapon : public Item {
+public:
+    enum Type {SWORD, AXE, POLEARM, STAFF, BOW};
+
+private:
+    Type type;
+    int attack;
+    int speed;
+
+public:
+    Weapon();
+    Weapon(string name, int price, int ID, Type type, int attack, int speed);
+    void setType(Type type);
+    void setAttack(int attack);
+    void setSpeed(int speed);
+    Type getType();
+    int getAttack();
+    int getSpeed();
 };
