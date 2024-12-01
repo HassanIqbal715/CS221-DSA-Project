4daee6669e6d11f18dce1338b2b816e0368e0fcd
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2024-12-01 18:26:32 +0500

    Created Inventory class

---
+++ include/Inventory.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Item.hpp"
+using namespace std;
+
+class Inventory {
+private:
+    LinkedList<Item*> items;
+
+public:
+    void insertItem();
+    Item* findItem(int ID);
+    void sort(); // Create this function Faiq!!
+    void print();
+};
