e99e082666c7184c678d20b65a78e7ad27ea5c81
Faiq Abdullah <faiqabdullah2b@gmail.com>
2024-12-22 01:24:10 +0500

    Added Different types of Skill and methods for new skillss

--- Skill.cpp
+++ Skill.cpp
@@ -1,17 +1,56 @@
 #include "include/Skill.hpp"
 
-Skill::Skill() { 
+Skill::Skill() {
+    ID = -1; 
     name = " ";
     description = " ";
-    buffFactor = 1;
+    skillFactor = 1;
 }
 
-Skill::Skill(string name, string descrption, int buffFactor) {
-    this->name = name;
-    this->description = description;
-    this->buffFactor = buffFactor;
-}
+Skill::Skill(int ID, string name, string descrption, int skillFactor) :
+    ID(ID), name(name), description(description), skillFactor(skillFactor){}
 
 Skill::~Skill() {
 
 }
+
+void Skill::setSkillFactor(int newFactor){
+        skillFactor = newFactor;
+}
+
+int Skill::getSkillFactor(){
+        return skillFactor;
+}
+
+void AttackSkill::attack(Enemy& enemy){
+        enemy.setHealth(enemy.getHealth() - skillFactor);
+}
+
+BuffSkill::BuffSkill() {
+    ID = -1; 
+    name = " ";
+    description = " ";
+    skillFactor = 1;
+    buffType = DEFENSE;
+}
+
+BuffSkill::BuffSkill(int ID, string name, string descrption, int skillFactor,
+    Buff buffType) : Skill(ID, name, description, skillFactor), 
+    buffType(buffType){}
+
+void BuffSkill::applySkill (Character& character){
+        switch(buffType){
+            case ATTACK:
+                character.setAttack(character.getAttack()*skillFactor);
+                break;
+            case DEFENSE:
+                character.setDefense(character.getDefense()*skillFactor);
+                break;
+            case HEALTH:
+                character.setHealth(character.getHealth()*skillFactor);
+                break;
+            case SPEED:
+                character.setSpeed(character.getSpeed()*skillFactor);
+                break;
+        }
+    }
--- include/Skill.hpp
+++ include/Skill.hpp
@@ -1,16 +1,58 @@
 #pragma once
 #include <iostream>
+#include "Character.hpp"
+#include "Enemy.hpp"
 using namespace std;
 
 class Skill {
-private:
+protected:
+
+    int ID;
+
     string name;
     string description;
-    int buffFactor;
+
+    // The skillFactor decides what amount of an action it will do
+    // For example, if its an attack skill, the skill factor would decide
+    // how much damage the skill would do. Similiarly for a buffing skill
+    // skill factor would decide how much to buff the parameter
+    int skillFactor;
+
 
 public:
-    void buffProperty(int &property);
+
     Skill();
-    Skill(string name, string descrption, int buffFactor);
+    Skill(int ID, string name, string descrption, int skillFactor);
+
+    void setSkillFactor(int newFactor);
+
+    int getSkillFactor();
+
     ~Skill();
+};
+
+class AttackSkill : public Skill{
+public:
+    void attack(Enemy& enemy){
+        enemy.setHealth(enemy.getHealth() - skillFactor);
+    }
+};
+
+class BuffSkill : public Skill{
+public:
+    enum Buff{ATTACK, HEALTH, DEFENSE, SPEED};
+
+protected:
+
+    Buff buffType;
+
+public:
+
+    
+    BuffSkill();
+    BuffSkill(int ID, string name, string descrption, int skillFactor,
+    Buff buffType);
+
+    void applySkill (Character& character);
+
 };
